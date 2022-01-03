d13d4df2ca904938dabe9e158a6fb8af984e1872
BeornOG <beornog@gmail.com>
2022-01-03 16:28:00 +0100

    added some work for the motion function

--- master/main.cpp
+++ master/main.cpp
@@ -1,10 +1,13 @@
 #include "master.h" // include the EtherCat Master library
 #include <ctime> 
 #include <cerrno>
+#include <chrono>
+#include <thread>
 
 //added Linux sleep function for ICP&B 
-void my_sleep(unsigned msec) {
+void delay(unsigned msec) {
 	//STEP7: create sleep msec timer 
+	std::this_thread::sleep_for(std::chrono::milliseconds(msec)); //lang leve stackoverflow antwoorden
 }
 
 int main(int argc, char* argv[])
@@ -13,17 +16,19 @@ int main(int argc, char* argv[])
 	//STEP1: select the proper EtherCAT network interface 
 	
    	//STEP2: instantiate the EtherCAT master object (with proper network interface & cycle time)
-
+	Master.Master();
 	//STEP3: check EtherCAT connection to EtherCAT slaves (and check OPERATIONAL state)
-   	if (1==1) { //NOTE: change me!!!!
+   	if (Master.connected) { 
+		   for (int i = 0; i < 65535; ++i){
+			   //STEP4: detect & determine the amount of connected slaves and RESET all drives
+			   Master.reset(i);
+		   }
+		//STEP5: enable the first slave (0 = EtherCAT master) && power the drive (OPERATIONAL state && powerstage enable)
+		Master.enable(1);
+		//STEP6: perform homing
+		Master.home(1, false)
+		//STEP7: assignment MOTION PROFILE
 
-    	//STEP4: detect & determine the amount of connected slaves and RESET all drives
-
-	//STEP5: enable the first slave (0 = EtherCAT master) && power the drive (OPERATIONAL state && powerstage enable)
-	
-  	//STEP6: perform homing
-
-	//STEP7: assignment MOTION PROFILE
 	/*
 	//MOTION PROFILE: assignment
 	//
