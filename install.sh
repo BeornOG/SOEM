ba27c747c7971b29c81f1c27f293ed246b8dd5aa
BeornOG <beornog@gmail.com>
2022-01-07 11:49:49 +0100

    added install.sh to make updating easier

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo"removing old SOEM"
+rm -rf SOEM
+echo "removed the old version of SOEM, downloading new version"
+git clone https://github.com/BeornOG/SOEM
+echo "downloaded the repository, using cmake to build included code to make the makefile"
+mkdir SOEM/build
+cd SOEM/build
+cmake ..
+echo "cmake complete, building main.cpp"
+cd master
+make
+echo "make finished"
