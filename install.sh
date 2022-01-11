1d64c29f5b3e914ba3c0f9fca6ebb3b8de29633f
BeornOG <beornog@gmail.com>
2022-01-11 10:54:00 +0100

    trying to fix colours in install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ RED='\033[0;31m'
 NC='\033[0m' # No Colour
 YELLOW='\033[1;33m'
 #make the message into a colour
-echo -e "${RED}removing old SOEM${NC}"
+echo -e "${RED}removing old SOEM ${NC}"
 rm -rf SOEM
-echo "${YELLOW}removed the old version of SOEM, downloading new version${NC}"
+echo " ${YELLOW}removed the old version of SOEM, downloading new version ${NC}"
 git clone https://github.com/BeornOG/SOEM
 echo "downloaded the repository, using cmake to build included code to make the makefile"
 cp SOEM/install.sh install.sh #update the installer
