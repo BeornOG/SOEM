282c7164b07643ba86445e9e4b2621fb6da4b121
BeornOG <beornog@gmail.com>
2022-01-12 16:22:25 +0100

    aowiuheiouh3r[o'h]A

--- master/main.cpp
+++ master/main.cpp
@@ -84,7 +84,7 @@ int main(int argc, char* argv[])
 			// extern Master EthCat;
 			int ErrorCode;
 			while(i < MotionData.size()-1) {
-				EthCat.movePosition(1, MotionData[i], true);
+				EthCat.movePosition(1, MotionData[i], false);
 				cout << "moving to: " << MotionData[i] << "\n";
 				delay(5000);
 				ErrorCode = EthCat.getError(1);
